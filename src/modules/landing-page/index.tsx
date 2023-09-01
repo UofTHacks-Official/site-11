2c9c15c81939d26275550acc5a336a72e974fa98
Gunin Wasan <guninw1001@gmail.com>
2023-08-31 21:43:01 -0400

    test deployment

--- src/modules/landing-page/index.tsx
+++ src/modules/landing-page/index.tsx
@@ -3,7 +3,7 @@ import { LandingPageContainer } from "./index.styles";
 const LandingPage = () => {
   return (
     <LandingPageContainer>
-      <h1>Landing Page</h1>
+      <h1>Test Deployment Links</h1>
     </LandingPageContainer>
   );
 };
