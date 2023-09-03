9a737a4908738aaa379cc5d27bc1ff3037e21d3f
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2023-09-03 09:08:19 -0400

    fixed hyperlink

--- src/components/faq-cards/index.tsx
+++ src/components/faq-cards/index.tsx
@@ -2,13 +2,13 @@ import { FAQCollectionContainer, FAQs } from "./index.styles";
 import React from "react";
 import { useMobileDetect } from "@/hooks/useMobileDetect";
 import Card from "./card";
+
+import Link from "next/link";
 interface FAQProps {
   faqs: any[];
 }
 
 const FAQCollection = ({ faqs }: FAQProps) => {
-  const isMobile = useMobileDetect();
-
   const faqList = faqs.map((faq, i) => (
     <Card index={i} question={faq.question} answer={faq.answer} />
   ));
@@ -22,12 +22,12 @@ const FAQCollection = ({ faqs }: FAQProps) => {
         answer={
           <span>
             Our policy will reflect the&nbsp;
-            {/* <Link
+            <Link
               type={"blue"}
               href="https://www.utoronto.ca/utogether/covid-19-planning-update"
             >
               University of Toronto&#39;s
-            </Link> */}
+            </Link>
             &nbsp;COVID protocols. The focus will be to ensure the event is a
             safe environment for all participants. We will have hand sanitizer
             stations, masks and disinfectant wipes throughout the venue.
