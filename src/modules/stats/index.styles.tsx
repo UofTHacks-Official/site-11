3e98cb64f8992a5c19fa3466b51a720ca1ab3c88
Gunin Wasan <guninw1001@gmail.com>
2023-09-27 00:01:32 -0400

    stats

--- src/modules/stats/index.styles.tsx
+++ src/modules/stats/index.styles.tsx
@@ -1,119 +1,11 @@
 import styled from "styled-components";
-import Inter from "@/components/inter";
 import Subjectivity from "@/components/subjectivity";
+import Image from "next/image";
 
 const StatsContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 10rem;
-`;
-
-type WrapperProps = {
-  mobile?: boolean | null;
-};
-
-const Wrapper = styled.div<WrapperProps>`
-  min-width: 100%;
-  min-height: 100%;
-  padding: 10%;
-  ${({ mobile }) =>
-    mobile &&
-    `
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  `}
-`;
-
-type StatsPhotoProps = {
-  justifyLeft?: boolean;
-  right?: boolean;
-  mobile?: boolean | null;
-};
-
-type Sticker = {
-  x?: string;
-  y?: string;
-  size?: string;
-  zIndex?: number;
-  opacity?: number;
-  rotate?: number;
-  backgroundColor?: string;
-};
-
-const StickerContainer = styled.div<Sticker>`
-  position: absolute;
-  object-fit: contain;
-  ${({ x }) => x && `left: ${x};`}
-  ${({ y }) => y && `top: ${y};`}
-    ${({ size }) => size && `width: ${size};`}
-    ${({ zIndex }) => zIndex && `z-index: ${zIndex};`}
-    ${({ opacity }) => opacity && `opacity: ${opacity};`} 
-    -webkit-user-select: none;
-  ${({ rotate }) => rotate && `transform: rotate(${rotate}deg);`}
-  touch-action: none;
-  -webkit-touch-callout: none;
-  -webkit-user-drag: none;
-  -khtml-user-drag: none;
-  -moz-user-drag: none;
-  -ms-user-drag: none;
-  user-drag: none;
-  ${({ backgroundColor }) =>
-    backgroundColor && `background-color: ${backgroundColor};`}
-  border-radius: 10px;
-  padding: 0.5rem 2rem;
-  box-shadow: 4px 4px 0px 0px #282828;
-`;
-
-const StatsPhoto = styled.div<StatsPhotoProps>`
-  ${({ mobile, right, justifyLeft }) =>
-    mobile
-      ? `
-    margin-bottom: 5rem;
-    `
-      : `
-    width: 40%;
-    margin-left: ${right ? "50vw" : "10%"};
-    margin-top: -10%;
-    margin-bottom: 2rem;
-    display: flex;
-    justify-content: ${justifyLeft ? "left" : "center"};
-    position: relative;
-  `};
-`;
-
-type BoxProps = {
-  backgroundColor: string;
-  rotation?: number;
-  zIndex?: number;
-};
-const BoxContainer = styled.div<BoxProps>`
-  width: 20rem;
-  height: 20rem;
-  flex-shrink: 0;
-  border-radius: 20px;
-  background-color: ${(props) => props.backgroundColor};
-
-  transform: rotate(${(props) => props.rotation || 0}deg);
-  z-index: ${(props) => props.zIndex || "auto"};
-
-  transition: transform 0.3s ease, box-shadow 0.3s ease;
-  &:hover {
-    transform: scale(1.1);
-    z-index: 100;
-    box-shadow: 10px #282828;
-  }
-  padding: 1rem;
-`;
-type InterProps = {
-  color?: string;
-};
-
-const StyledInter = styled(Inter)<InterProps>`
-  font-size: 24px;
-  color: ${(props) => props.color};
 `;
 
 const StyledSubjectivity = styled(Subjectivity)`
@@ -128,13 +20,41 @@ const ColourText = styled.span`
   color: #007d7d;
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
+
+const SubContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledImage = styled(Image)`
+  max-width: 100%;
+  max-height: 100%;
+  aspect-ratio: 1;
+
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: none;
+  -webkit-touch-callout: none;
+  -webkit-user-drag: none;
+  -khtml-user-drag: none;
+  -moz-user-drag: none;
+  -ms-user-drag: none;
+  user-drag: none;
+`;
+
 export {
   StatsContainer,
-  Wrapper,
-  StatsPhoto,
-  BoxContainer,
-  StickerContainer,
-  StyledInter,
   StyledSubjectivity,
   ColourText,
+  Container,
+  SubContainer,
+  StyledImage,
 };
--- src/modules/stats/index.tsx
+++ src/modules/stats/index.tsx
@@ -1,85 +1,32 @@
-import { useMobileDetect } from "@/hooks/useMobileDetect";
 import {
   StatsContainer,
-  Wrapper,
-  StatsPhoto,
-  BoxContainer,
-  StickerContainer,
-  StyledInter,
   StyledSubjectivity,
   ColourText,
+  Container,
+  SubContainer,
+  StyledImage,
 } from "./index.styles";
+import StatHacker from "public/assets/stats/Stat_Hacker.png";
+import StatPrizes from "public/assets/stats/Stat_Prizes.png";
+import StatProject from "public/assets/stats/Stat_Project.png";
+import StatSchools from "public/assets/stats/Stat_Schools.png";
 
 const Stats = () => {
-  const isMobile = useMobileDetect();
-
   return (
     <StatsContainer>
       <StyledSubjectivity>
         Last Year’s <ColourText>Statistics</ColourText>
       </StyledSubjectivity>
-      <Wrapper mobile={isMobile}>
-        <StatsPhoto mobile={isMobile}>
-          <BoxContainer backgroundColor={"#f0a0c8"}>
-            <StickerContainer
-              backgroundColor={"#007D7D"}
-              x={isMobile ? "10%" : "30%"}
-              y={"85%"}
-              rotate={-14}
-              size="17rem"
-            >
-              <StyledInter fontStyle={"semibold"} color={"#F9F9F9"}>
-                90+ Projects
-              </StyledInter>
-            </StickerContainer>
-          </BoxContainer>
-        </StatsPhoto>
-        <StatsPhoto mobile={isMobile} right>
-          <BoxContainer backgroundColor={"#a050a0"}>
-            <StickerContainer
-              backgroundColor={"#F0C800"}
-              x={isMobile ? "10%" : "30%"}
-              y={"90%"}
-              rotate={7.5}
-              size="17rem"
-            >
-              <StyledInter fontStyle={"semibold"} color={"#282828"}>
-                25+ Schools
-              </StyledInter>
-            </StickerContainer>
-          </BoxContainer>
-        </StatsPhoto>
-        <StatsPhoto mobile={isMobile} justifyLeft>
-          <BoxContainer backgroundColor={"#2850A0"}>
-            <StickerContainer
-              backgroundColor={"#F0A000"}
-              x={isMobile ? "10%" : "-20%"}
-              y={"90%"}
-              rotate={isMobile ? 0 : 11.3}
-              size="17rem"
-            >
-              <StyledInter fontStyle={"semibold"} color={"#282828"}>
-                $18k+ in Prizes
-              </StyledInter>
-            </StickerContainer>
-          </BoxContainer>
-        </StatsPhoto>
-        <StatsPhoto mobile={isMobile} right justifyLeft>
-          <BoxContainer backgroundColor={"#F0A000"}>
-            <StickerContainer
-              backgroundColor={"#A050A0"}
-              x={"10%"}
-              y={"95%"}
-              rotate={-6}
-              size="17rem"
-            >
-              <StyledInter fontStyle={"semibold"} color={"#F9F9F9"}>
-                600+ Hackers
-              </StyledInter>
-            </StickerContainer>
-          </BoxContainer>
-        </StatsPhoto>
-      </Wrapper>
+      <Container>
+        <SubContainer>
+          <StyledImage src={StatHacker} alt={"Stat Hacker"} />
+          <StyledImage src={StatPrizes} alt={"Stat Prizes"} />
+        </SubContainer>
+        <SubContainer>
+          <StyledImage src={StatProject} alt={"Stat Project"} />
+          <StyledImage src={StatSchools} alt={"Stat Schools"} />
+        </SubContainer>
+      </Container>
     </StatsContainer>
   );
 };
