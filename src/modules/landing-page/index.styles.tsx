ded392774a65b6a1218161e6e7b0896ed498640e
Gunin Wasan <guninw1001@gmail.com>
2023-10-03 00:36:34 -0400

    landing banner image changes

--- src/modules/landing-page/index.styles.tsx
+++ src/modules/landing-page/index.styles.tsx
@@ -7,6 +7,8 @@ const LandingPageContainer = styled.div`
   width: 100vw;
   min-width: 100%;
   justify-content: center;
+  height: 90vh;
+  max-height: 900px;
 `;
 
 export { LandingPageContainer };
