fee66bcca4fa72770f16155b1f5eaf058d425e2e
Gunin Wasan <guninw1001@gmail.com>
2023-10-03 20:13:01 -0400

    updated stats

--- src/modules/stats/index.styles.tsx
+++ src/modules/stats/index.styles.tsx
@@ -35,8 +35,8 @@ const SubContainer = styled.div`
 `;
 
 const StyledImage = styled(Image)`
-  max-width: 100%;
-  max-height: 100%;
+  width: 100%;
+  height: 100%;
   aspect-ratio: 1;
   object-fit: contain;
   user-select: none;
