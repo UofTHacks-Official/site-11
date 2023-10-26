07e32821f02f2de4751c5c347b7346f6eb5d9aa0
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2023-10-26 00:36:57 -0400

    added mlh banner

---
+++ src/components/mlh-banner/index.styles.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+type MLHContainerProps = {
+  isMobile?: boolean | null;
+};
+const MLHContainer = styled.a<MLHContainerProps>`
+  display: block;
+  max-width: 100px;
+  min-width: 60px;
+  position: fixed;
+  right: ${(props) => (props.isMobile ? "100px" : "25px")};
+  top: 0;
+  width: 10%;
+  z-index: 10000;
+`;
+
+const MLHWrapper = styled.svg`
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-image: url("/mlh.svg");
+`;
+
+export { MLHContainer, MLHWrapper };
